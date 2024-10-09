9672ab574185352919b8cc571073eae59c357055
s0037493 <s0037493@wyke.ac.uk>
2024-10-09 08:32:51 +0000

    can find vector from one player to another

--- player.js
+++ player.js
@@ -111,14 +111,44 @@ class Player extends Box {
     }
   }
 
-
   pTpAngle(playerA, playerB){
+    let Aposition = []
+    let Bposition = []
+
     if(playerA=="a"){
-      let Aposition = [user.getX(), user.getY(), user.getZ()]
+      Aposition = [user.getX(), user.getY(), user.getZ()]
     }
     else if(playerA=="b"){
-      let Aposition = [user.getX(), user.getY(), user.getZ()]
+      Aposition = [ai[0].getX(), ai[0].getY(), ai[0].getZ()]
     }
+    else if(playerA=="c"){
+      Aposition = [ai[1].getX(), ai[1].getY(), ai[1].getZ()]
+    }
+    else if(playerA=="d"){
+      Aposition = [ai[2].getX(), ai[2].getY(), ai[2].getZ()]
+    }
+
+    if(playerB=="a"){
+      Bposition = [user.getX(), user.getY(), user.getZ()]
+    }
+    else if(playerB=="b"){
+      Bposition = [ai[0].getX(), ai[0].getY(), ai[0].getZ()]
+    }
+    else if(playerB=="c"){
+      Bposition = [ai[1].getX(), ai[1].getY(), ai[1].getZ()]
+    }
+    else if(playerB=="d"){
+      Bposition = [ai[2].getX(), ai[2].getY(), ai[2].getZ()]
+    }
+
+    let AB = []
+    for(let i=0; i<3; i++){
+      AB[i] = Bposition[i] - Aposition[i]
+    }
+    console.log(AB)    
+    
+
+
   }
   
   }
